7d4214a9ae084e22bbc9788151e6185bba30d47f
ade-damilola <oluwatobilobaadedigba73@gmail.com>
2024-02-22 09:53:43 +0100

    Initial commit: Add HTML and CSS files

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // You can add dynamic behavior here
+  console.log('Document is ready!');
+});
+
+
+// script.js
+alert("Hello from JavaScript!");
+
+
